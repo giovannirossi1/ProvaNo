a8805650ece9e7785a10acd91a5ed84d81ab9ece
giovannirossi1 <73940903+giovannirossi1@users.noreply.github.com>
2021-01-28 13:22:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var http = require("http");
 var express = require("express");
+var apiServer=express();
 
 var hostname = "127.0.0.1";
 var port = 3000;
+var nome="NODE"
 
-var server = http.createServer((req, res) => {
-    console.log("req:", req);
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Ciao belloccio!");
+
+
+apiServer.listen(port, hostname, () => {
+    console.log("server running at http://%s:%d/", hostname, port);
 });
 
-server.listen(port, hostname, () => {
-    console.log("server running at http://%s:%d/", hostname, port);
+apiServer.get("nome",(req, res)=>{
+   console.log("ho ricevuto una get su nome");
+   res.send("ciao, il nome del server è:"+nome);
+});
+apiServer.get("/", function(req, res){
+    res.send("sei in home");
+});
+
+apiServer.apiServer.get("/index",(req, res)=>{
+    res.sendFile("C:\Users\rossi.giovanniangelo\Desktop\provaNode-1\\index.html");
 });
